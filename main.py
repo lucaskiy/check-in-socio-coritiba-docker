f18467b8df65733a06ee2136fc4ab520a715d96e
Lucas Kiy <lucask.kiy@gmail.com>
2024-10-10 00:59:34 -0300

    dev: changed logs to logging

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-import time
-import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+
 import smtplib
-from dotenv import load_dotenv
-from utils.logger_print import print_log
+import time
+import os
+import logging 
+
+logging.basicConfig(
+    level=logging.INFO,  
+    format='%(asctime)s - %(levelname)s - %(message)s', 
+    datefmt='%Y-%m-%d %H:%M:%S'  
+)
 
 
 class CoxaCheckIn:
@@ -24,7 +29,7 @@ class CoxaCheckIn:
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--headless")  # option to not open google chrome window
         self.browser = webdriver.Chrome(options=chrome_options)
-        print(print_log("Browser aberto!"))
+        logging.info("Browser aberto!")
 
 
     def lets_checkin(self) -> None:
@@ -33,12 +38,12 @@ class CoxaCheckIn:
         """
         # open Coritiba website
         self.browser.get("https://sociocoxabranca.coritiba.com.br/") 
-        print(print_log("Site do Coritiba aberto!"))
+        logging.info("Site do Coritiba aberto!")
         time.sleep(1)
 
         login_success = self.login_socios_page()
         if login_success:
-            print(print_log("Login realizado com sucesso, seguindo para o check-in"))
+            logging.info("Login realizado com sucesso, seguindo para o check-in")
 
             #click on make check-in option
             button = self.browser.find_element(By.XPATH, '//*[@id="conteudo_hotsite"]/div/div[2]/div[1]/div/div[6]')
@@ -51,12 +56,12 @@ class CoxaCheckIn:
         png_file_path = f"./check-in-screenshots/check-in-proof-{datetime.strftime(datetime.now(), '%Y%m%d_%H%M')}.png"
         self.browser.save_screenshot(png_file_path)
 
-        print(print_log("O seu check-in para o próximo jogo do Coxa-doido foi feito com sucesso"))
+        logging.info("O seu check-in para o próximo jogo do Coxa-doido foi feito com sucesso")
         time.sleep(2)
         self.browser.quit()
-        print(print_log("Browser fechado!"))
+        logging.info("Browser fechado!")
 
-        print(print_log("Preparando envio de confirmação via e-mail"))
+        logging.info("Preparando envio de confirmação via e-mail")
         self.send_email_notification(png_file_path)
 
 
@@ -95,10 +100,10 @@ class CoxaCheckIn:
             if len(cpf) == 11:
                 return True
             else:
-                print(print_log("Quantidade de dígitos do CPF inválido, por favor digite 11 números"))
+                logging.error("Quantidade de dígitos do CPF inválido, por favor digite 11 números")
                 return False
         else:
-            print(print_log("Carácter inválido, por favor digite somente números"))
+            logging.error("Carácter inválido, por favor digite somente números")
 
 
     def select_stadium_sector(self):
@@ -111,12 +116,12 @@ class CoxaCheckIn:
 
         if sector_to_sit == "arquibancada":
             self.browser.find_element(By.XPATH, '/html/body/div[9]/div/div/div/div/div[2]/div[8]/div/div/div[2]/div/div/div[3]/div/div[2]').click()
-            print(print_log("Realizado check-in na Arquibancada!"))
+            logging.info("Realizado check-in na Arquibancada!")
             time.sleep(1)
 
         elif sector_to_sit == "maua":
             self.browser.find_element(By.XPATH, '/html/body/div[9]/div/div/div/div/div[2]/div[8]/div/div/div[2]/div/div/div[2]/div/div[2]').click()
-            print(print_log("Realizado check-in na Mauá!"))
+            logging.info("Realizado check-in na Mauá!")
             time.sleep(1)
 
         else:
@@ -136,14 +141,14 @@ class CoxaCheckIn:
             target_element = self.browser.find_element(By.XPATH, '/html/body/div[10]/div[2]/div/div/div/div[2]')
             self.browser.execute_script("arguments[0].scrollIntoView();", target_element) # the scroll is necessary because the button is hidden behind a pop-up
             self.browser.find_element(By.XPATH, '/html/body/div[10]/div[2]/div/div/div/div[2]').click()
-            print(print_log("Check-in feito para a carteirinha fisica!"))
+            logging.info("Check-in feito para a carteirinha fisica!")
             time.sleep(5)
 
         elif checkin_type == "online":
             target_element = self.browser.find_element(By.XPATH, '/html/body/div[10]/div[2]/div/div/div/div[3]')
             self.browser.execute_script("arguments[0].scrollIntoView();", target_element) # the scroll is necessary because the button is hidden behind a pop-up
             self.browser.find_element(By.XPATH, '/html/body/div[10]/div[2]/div/div/div/div[3]').click()
-            print(print_log("Check-in feito para a carteirinha online!"))
+            logging.info("Check-in feito para a carteirinha online!")
             time.sleep(5)
 
         else:
@@ -176,7 +181,7 @@ class CoxaCheckIn:
             server.login(msg['From'], password)
             server.sendmail(msg['From'], msg['To'], msg.as_string())
             server.quit()
-            print(print_log(f"Email de confirmação enviado com sucesso para {msg['To']}:"))
+            logging.info("Email de confirmação enviado com sucesso para {msg['To']}:")
 
         except Exception as e:
             raise e
